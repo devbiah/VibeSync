69fc857027ed8d71f66835a46c6408e5ea6c32ed
devbiah <emmyisilva@gmail.com>
2024-10-21 13:27:16 -0300

    feat:starting frontend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,25 @@
-import {createServer} from 'node:http'
+import Express from "express"
 
-const servidor = createServer((req,res) =>{
-    console.log('qualquer coisa')
-    res.write('ta funcionando 2')
-    return res.end()
+const app = Express()
+app.use(Express.json())
+
+app.get('/get', function(req, res){
+    res.send('Enviar esta mensagem')
 })
 
-servidor.listen(8000)
+app.post('/register', function(req, res){
+    try{
+        const {nome, sobrenome, email, senha, nascimento} = req.body
+        if(!nome || !sobrenome || !email || !senha || !nascimento){
+            res.status(406).send("Todos os campos devem ser enviados")
+            return
+        } 
+
+        res.status(201).send("Usuário criado")
+        return
+    } catch(error){
+        res.send("OK")
+    }
+})
+
+app.listen(8000)
--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,12 +1,48 @@
 import React from "react";
-import WebRoutes from "../routes/index"
+import { Text, View, StyleSheet, Pressable, Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-function App() {
-  return (
-    <div className="App">
-      <WebRoutes />
-    </div>
-  );
-}
+const firstIcon = require("../assets/img/firstIcon.svg"); 
 
-export default App;
+export default FirstPage = () => {
+
+    const handleSignUp = () => {
+        navigation.navigate("Login"); 
+    };
+
+    return (
+        <LinearGradient colors={["#6D8299", "#242B33"]} style={styles.gradient}>
+            <View style={styles.container}>
+                <View>
+                    <Image source={firstIcon} style={styles.img} /> 
+                </View>
+
+                <Pressable style={styles.button} onPress={handleSignUp}>
+                    <Text style={{ color: '#242B33', fontSize: 25 }}>Join</Text>
+                </Pressable>
+            </View>
+        </LinearGradient>
+    );
+};
+
+const styles = StyleSheet.create({
+    gradient: {
+        flex: 1,
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 100,
+    },
+    button: {
+        backgroundColor: '#B4C6D7',
+        width: 303,
+        height: 58,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 89,
+        marginTop: 200,
+    },
+});
